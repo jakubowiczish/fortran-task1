d8171081919a1928640405bbce02dcb3c8342689
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-04-26 17:50:29 +0200

    Add working multiplications for kind4, kind8 and kind16

--- src/main.F90
+++ src/main.F90
@@ -1,35 +1,107 @@
 program main
     use naivemath
 
-    real (kind = 4), allocatable, dimension(:, :) :: A, B
-    real (kind = 4), allocatable, dimension(:, :) :: C
+    real (kind = 4), allocatable, dimension(:, :) :: A4, B4
+    real (kind = 4), allocatable, dimension(:, :) :: C4
+
+    real (kind = 8), allocatable, dimension(:, :) :: A8, B8
+    real (kind = 8), allocatable, dimension(:, :) :: C8
+    
+    real (kind = 16), allocatable, dimension(:, :) :: A16, B16
+    real (kind = 16), allocatable, dimension(:, :) :: C16
 
     integer (kind = 4) :: n
-    ! real (kind = 4) :: k
+    
+    real (kind = 4) :: randomNumber4
+    real (kind = 8) :: randomNumber8
+    real (kind = 16) :: randomNumber16
 
+    ! ####################################################################
+    !                              KIND 4 
+    ! ####################################################################
     n = 10
-    
     do while(n <= 1280)
-        allocate(A(n, n))
-        allocate(B(n, n))
-        allocate(C(n, n))
+        allocate(A4(n, n))
+        allocate(B4(n, n))
+        allocate(C4(n, n))
 
-        A = 0.9
-        B = 0.9
-    
-        C = 0
-        C = naivmull(A, B)
+        call random_number(randomNumber4)
 
-        write(*,*) C
+        A4 = randomNumber4
+        B4 = randomNumber4
     
-        if(allocated(A)) deallocate(A)
-        if(allocated(B)) deallocate(B)
-        if(allocated(C)) deallocate(C)
+        C4 = 0
+        C4 = naivmull(A4, B4)
+
+        write(*,*) C4
+    
+        if(allocated(A4)) deallocate(A4)
+        if(allocated(B4)) deallocate(B4)
+        if(allocated(C4)) deallocate(C4)
 
         write(*,*) "size: "
         write(*,*) n
 
-        n = n*2
+        n = n * 2
+    end do
+
+    ! ####################################################################
+    !                              KIND 8 
+    ! ####################################################################
+    
+    n = 10
+    do while(n <= 1280)
+        allocate(A8(n, n))
+        allocate(B8(n, n))
+        allocate(C8(n, n))
+
+        call random_number(randomNumber8)
+        A8 = randomNumber8
+        B8 = randomNumber8
+    
+        C8 = 0
+        C8 = naivmull(A8, B8)
+
+        write(*,*) C8
+    
+        if(allocated(A8)) deallocate(A8)
+        if(allocated(B8)) deallocate(B8)
+        if(allocated(C8)) deallocate(C8)
+
+        write(*,*) "size: "
+        write(*,*) n
+
+        n = n * 2
+    end do
+
+
+    ! ####################################################################
+    !                              KIND 16 
+    ! ####################################################################
+
+    n = 10
+    do while(n <= 1280)
+        allocate(A16(n, n))
+        allocate(B16(n, n))
+        allocate(C16(n, n))
+
+        call random_number(randomNumber16)
+        A16 = randomNumber16
+        B16 = randomNumber16
+    
+        C16 = 0
+        C16 = naivmull(A16, B16)
+
+        write(*,*) C16
+    
+        if(allocated(A16)) deallocate(A16)
+        if(allocated(B16)) deallocate(B16)
+        if(allocated(C16)) deallocate(C16)
+
+        write(*,*) "size: "
+        write(*,*) n
+
+        n = n * 2
     end do
     
 
--- src/naivemath.F90
+++ src/naivemath.F90
@@ -1,9 +1,9 @@
 module naivemath
 
     public :: naivmull
-    private naivmullKind4 !, naivmullKind8
+    private naivmullKind4, naivmullKind8, naivmullKind16
     interface naivmull
-        procedure naivmullKind4 !, naivmullKind8
+        procedure naivmullKind4, naivmullKind8, naivmullKind16
     end interface
 
     contains
@@ -17,6 +17,8 @@ module naivemath
         allocate(C(size(A, 1), size(B, 2)))
         C = 0
 
+        write(*,*) "NAIVMULL KIND 4"
+
         do i = 1, size(A, 1)
             do j = 1, size(B, 2)
                 do k = 1, size(A, 2)
@@ -24,26 +26,48 @@ module naivemath
                 end do
             end do
         end do
-
     end function
 
 
-    ! function naivmullKind8(A, B) result(C)
-    !     implicit none
-    !     real (kind = 8), intent(in), dimension(:, :) :: A, B
-    !     real (kind = 8), dimension(size(A,1), size(B, 2)) :: C
-    !     integer (kind = 8) :: i, j, k
+    function naivmullKind8(A, B) result(C)
+        implicit none
+        real (kind = 8), intent(in), dimension(:, :) :: A, B
+        real (kind = 8), allocatable, dimension(:, :) :: C
+        integer (kind = 8) :: i, j, k
         
-    !     C = 0
+        allocate(C(size(A, 1), size(B, 2)))
+        C = 0
 
-    !     do i = 1, size(A, 1)
-    !         do j = 1, size(B, 2)
-    !             do k = 1, size(A, 2)
-    !                 C(i, j) = C(i, j) + A(i, k) * B(k, j)
-    !             end do
-    !         end do
-    !     end do
-    ! end function
+        write(*,*) "NAIVMULL KIND 8"
 
+        do i = 1, size(A, 1)
+            do j = 1, size(B, 2)
+                do k = 1, size(A, 2)
+                    C(i, j) = C(i, j) + A(i, k) * B(k, j)
+                end do
+            end do
+        end do
+    end function
+
+
+    function naivmullKind16(A, B) result(C)
+        implicit none
+        real (kind = 16), intent(in), dimension(:, :) :: A, B
+        real (kind = 16), allocatable, dimension(:, :) :: C
+        integer (kind = 8) :: i, j, k
+        
+        allocate(C(size(A, 1), size(B, 2)))
+        C = 0
+
+        write(*,*) "NAIVMULL KIND 16"
+
+        do i = 1, size(A, 1)
+            do j = 1, size(B, 2)
+                do k = 1, size(A, 2)
+                    C(i, j) = C(i, j) + A(i, k) * B(k, j)
+                end do
+            end do
+        end do
+    end function
 
 end module
